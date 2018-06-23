342e13702ab6046a5d0d2cffde23fc5c36ea3229
FilipSavic95 <filipsavic1995@yahoo.com>
2018-06-23 22:26:56 +0200

    Fixed whole page reloading.

--- project/src/app/shared/shared.module.ts
+++ project/src/app/shared/shared.module.ts
@@ -4,13 +4,19 @@ import { PipesModule } from './pipes/pipes.module';
 import { NavbarComponent } from './navbar/navbar.component';
 import {LogsListComponent} from './logs-list/logs-list.component';
 import { ToasterAlarmsComponent } from './toaster-alarms/toaster-alarms.component';
+import {FormsModule} from '@angular/forms';
+import {LogsService} from './logs.service';
+import {AppRoutingModule} from '../app-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    AppRoutingModule,
     PipesModule
   ],
   exports: [ PipesModule, NavbarComponent, ToasterAlarmsComponent ],
-  declarations: [NavbarComponent, LogsListComponent, ToasterAlarmsComponent] // imports and exports https://stackoverflow.com/a/41281872/4345461
+  declarations: [NavbarComponent, LogsListComponent, ToasterAlarmsComponent], // imports/exports https://stackoverflow.com/a/41281872/4345461
+  providers: [LogsService]
 })
 export class SharedModule { }
