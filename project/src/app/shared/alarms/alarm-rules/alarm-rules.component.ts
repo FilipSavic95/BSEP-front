94124b4085005cdd47e698360bab3ee497cf0a65
FilipSavic95 <filipsavic1995@yahoo.com>
2018-06-24 20:48:16 +0200

    Added alarm rules component.

--- project/src/app/shared/alarms/alarm-rules/alarm-rules.component.ts
+++ project/src/app/shared/alarms/alarm-rules/alarm-rules.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Page} from '../../../model/Page';
+import {AlarmsService} from '../alarms.service';
 
 @Component({
   selector: 'app-alarm-rules',
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlarmRulesComponent implements OnInit {
 
-  constructor() { }
+  alarms: Page;
+
+  page = 0;
+  size = 8;
+
+  loading: boolean;
+  errorMessage = '';
+
+  constructor(private alarmsService: AlarmsService) { }
 
   ngOnInit() {
+    this.getAlarms();
+  }
+
+  getAlarms() {
+    this.loading = true;
+    this.errorMessage = '';
+    this.alarmsService.getAlarmRules(this.page, this.size)
+      .subscribe(
+        resp => {
+          this.alarms = resp;
+          // this.alarms.content.map(alarm => {
+          //   if (alarm.date !== null) {
+          //     alarm.date = alarm.date.split('T').join(' ');
+          //   }
+          // });
+          this.loading = false;
+        },
+        err => {
+          console.log(err);
+          this.errorMessage = err.error.message;
+          this.loading = false;
+        }
+      );
+  }
+
+
+  nextAlarmsPage() {
+    this.page++;
+    this.getAlarms();
+  }
+
+  previousAlarmsPage() {
+    this.page--;
+    this.getAlarms();
   }
 
 }
