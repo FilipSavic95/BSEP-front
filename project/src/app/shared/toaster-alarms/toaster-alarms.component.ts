e9dcab53cb07234c8eba63e7e553d38dc70ab342
Aleksandar Savic <alek_bn@hotmail.com>
2018-08-17 12:52:44 +0200

    secure remote communication + sockets

--- project/src/app/shared/toaster-alarms/toaster-alarms.component.ts
+++ project/src/app/shared/toaster-alarms/toaster-alarms.component.ts
@@ -21,7 +21,7 @@ export class Message {
   styleUrls: ['./toaster-alarms.component.css']
 })
 export class ToasterAlarmsComponent implements OnInit {
-  private serverUrl = 'https://localhost:8765/socket';
+  private serverUrl = 'https://192.168.1.10:8765/socket';
   private stompClient;
 
   messages: Message[] = [
